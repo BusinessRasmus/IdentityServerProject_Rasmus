9b2c3fdc169a19e67847bee0f94e17edb417d0e7
BusinessRasmus <143530718+BusinessRasmus@users.noreply.github.com>
2024-05-13 21:27:30 +0200

    Added DbContext-OnModelCreating

--- IdentityServerProject_Rasmus/Data/ApplicationDbContext.cs
+++ IdentityServerProject_Rasmus/Data/ApplicationDbContext.cs
@@ -1,3 +1,4 @@
+using Humanizer;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,6 +6,13 @@ namespace IdentityServerProject_Rasmus.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
 
+            builder.Entity<ApplicationUser>(b =>
+                b.HasMany(e => e.UserRoles)
+            ); //TODO Keep adding functionality.
+        }
     }
 }
--- IdentityServerProject_Rasmus/Data/ApplicationUser.cs
+++ IdentityServerProject_Rasmus/Data/ApplicationUser.cs
@@ -5,6 +5,20 @@ namespace IdentityServerProject_Rasmus.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public virtual ICollection<ApplicationUserRole> UserRoles { get; set; }
+    }
+
+    public class ApplicationUserRole : IdentityUserRole<string>
+    {
+        public virtual ApplicationUser User { get; set; }
+
+        public virtual ApplicationRole Role { get; set; }
+
+    }
+
+    public class ApplicationRole : IdentityRole
+    {
+        public virtual ICollection<ApplicationUserRole> Roles { get; set; }
     }
 
 }
